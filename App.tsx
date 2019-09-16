27bb5cc5a61d3fdf040386120088eeec3244fb65
Josh Rymer <jrymer1@gmail.com>
2019-09-16 16:09:06 -0400

    Added typescript

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react'
+import {
+  View,
+  Text
+} from 'react-native';
+
+class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          App
+        </Text>
+      </View>
+      );
+  }
+};
+
+export default App;
